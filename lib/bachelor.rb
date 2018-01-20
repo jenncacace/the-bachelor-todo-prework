09d6e229ed47fd864994a43d57afca06dbd13b5a
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-20 18:23:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season]. each do |contestant_hash, value|
     if contestant_hash["status"] == "Winner"
-      return contestant_hash["name"] 
+      return contestant_hash["name"].split(" ") 
     end
   end
 end
